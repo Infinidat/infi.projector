8fbe1fb6afb160301be0bd7b56e0aefdb74fd688
Guy Rozendorn <guy@rzn.co.il>
2012-08-22 17:51:58 +0300

    TRIVIAL removing unused code

--- src/infi/projector/plugins/builtins/__init__.py
+++
@@ -1,6 +0,0 @@
-
-def get_all():
-    from .repository import RepositoryPlugin
-    from .build import BuildPlugin
-    from .version import VersionPlugin
-    return [RepositoryPlugin, BuildPlugin, VersionPlugin]
