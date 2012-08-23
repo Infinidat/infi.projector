9b2a6bd12dc1208ee76066be5ed60dfa0af0a6e7
Guy Rozendorn <guy@rzn.co.il>
2012-08-23 16:34:21 +0300

    TRIVIAL isolated-python command

---
+++ tests/test_command__isolated_python.py
@@ -0,0 +1,9 @@
+from .test_case import TestCase
+from infi.unittest.parameters import iterate
+
+class IsolatedPythonVersion(TestCase):
+    def test(self):
+        with self.temporary_directory_context():
+            self.projector("repository init a.b.c none short long")
+            self.projector("isolated-python python-version get")
+            self.projector("isolated-python python-version set v2.7.2.5")
