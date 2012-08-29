e04a2b23b6c5296c3c2f519e2d654e6cf932c12d
Guy Rozendorn <guy@rzn.co.il>
2012-08-29 20:43:42 +0300

    TRIVIAL using infi.recipe.console_scripts

--- src/infi/projector/plugins/builtins/devenv/__init__.py
+++ src/infi/projector/plugins/builtins/devenv/__init__.py
@@ -84,7 +84,7 @@ class DevEnvPlugin(CommandPlugin):
         return sections[0]
 
     def create_scripts(self):
-        self.install_sections_by_recipe("infi.vendata.console_scripts")
+        self.install_sections_by_recipe("infi.recipe.console_scripts")
 
     def clean_build(self):
         from os.path import exists
