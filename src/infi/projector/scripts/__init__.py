614237d4876182036ba6c3bfd1164a728a431a84
Guy Rozendorn <guy@rzn.co.il>
2012-10-12 12:07:55 +0200

    TRIVIAL logging to stderr, level will be DEBUG if DEBUG env-var is set

--- src/infi/projector/scripts/__init__.py
+++ src/infi/projector/scripts/__init__.py
@@ -3,7 +3,9 @@ from sys import argv
 #pylint: disable=R0801
 
 def projector(argv=argv[1:]):
-    from logging import basicConfig, INFO
+    from os import environ
+    from logging import basicConfig, INFO, DEBUG
+    from sys import stderr
     from infi.projector.commandline_parser import parse_commandline_arguments
-    basicConfig(level=INFO)
+    basicConfig(level=DEBUG if environ.get("DEBUG") else INFO, stream=stderr)
     parse_commandline_arguments(argv)
