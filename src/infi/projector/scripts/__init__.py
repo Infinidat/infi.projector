0f0fe1508143d8dca992a66d63006523dac825f2
Guy Rozendorn <guy@rzn.co.il>
2012-10-17 14:07:27 +0200

    TRIVIAL change log message from info to debug

--- src/infi/projector/scripts/__init__.py
+++ src/infi/projector/scripts/__init__.py
@@ -7,5 +7,5 @@ def projector(argv=argv[1:]):
     from sys import stderr
     from infi.projector.commandline_parser import parse_commandline_arguments
     basicConfig(level=DEBUG if environ.get("DEBUG") else INFO, stream=stderr)
-    getLogger(__name__).info(' '.join(['projector'] + argv))
+    getLogger(__name__).debug(' '.join(['projector'] + argv))
     parse_commandline_arguments(argv)
