f48b8090f0c91c4a1da337d79ac34a5174523a7b
Guy Rozendorn <guy@rzn.co.il>
2012-11-15 13:50:13 +0200

    TRIVIAL revert_if_failed fix

--- src/infi/projector/helper/utils/__init__.py
+++ src/infi/projector/helper/utils/__init__.py
@@ -226,10 +226,10 @@ def revert_if_failed(keep_leftovers):
         if keep_leftovers:
             raise
         now = get_status()
-        for tag in set(after['tags']).difference(set(before['tags'])):
-            repository.delete(after[tag])
-        for branch in set(after['branches']).difference(set(before['branches'])):
-            repository.delete(after[branch])
+        for tag in set(now['tags']).difference(set(before['tags'])):
+            repository.delete(now[tag])
+        for branch in set(now['branches']).difference(set(before['branches'])):
+            repository.delete(now[branch])
         for branch_name in ['master', 'develop']:
             repository.resetHard()
             branch = repository.getBranchByName(branch_name)
