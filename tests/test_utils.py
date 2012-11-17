419197406d497f669094f51f5e425a6b276fc40e
Guy Rozendorn <guy@rzn.co.il>
2012-11-17 11:34:30 +0200

    TRIVIAL fixed test UtilsTestCase.test_execute_assert_success

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,12 +2,11 @@ from .test_case import TestCase
 
 class UtilsTestCase(TestCase):
     def test_execute_assert_success(self):
-        from infi.projector.helper.utils import execute_with_buildout
-        from infi.execute import ExecutionError
+        from infi.projector.helper.utils import execute_with_buildout, PrettyExecutionError
         try:
             execute_with_buildout(["install", "non-existing-section"])
-        except ExecutionError, err:
-            self.assertFalse(r"\n" not in str(err))
+        except PrettyExecutionError, err:
+            self.assertFalse(r"\n" in str(err))
 
     def test_revert_if_failed(self):
         from infi.projector.helper.utils import revert_if_failed
