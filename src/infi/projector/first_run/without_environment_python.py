b8ec5b845868dc81827a4d5cd34c529ea493e010
Guy Rozendorn <guy@rzn.co.il>
2013-03-29 12:14:30 +0300

    TRIVIAL without_environment_python: using buildout-1.6.3 explicitly

--- src/infi/projector/first_run/without_environment_python.py
+++ src/infi/projector/first_run/without_environment_python.py
@@ -5,12 +5,12 @@ USE_ISOLATED_PYTHON = '--use-isolated-python' in sys.argv
 IN_VIRTUALENV = hasattr(sys, 'real_prefix')
 BUILDOUT = path.join("bin", "buildout")
 COMMANDS = [
-            "python {0} bootstrap.py -d".format('' if IN_VIRTUALENV else '-S'),
+            "python {0} bootstrap.py -d -v 1.6.3".format('' if IN_VIRTUALENV else '-S'),
             "{0} -s buildout:develop= install setup.py __version__.py".format(BUILDOUT),
             "{0} -s install development-scripts".format(BUILDOUT)
            ]
 if USE_ISOLATED_PYTHON:
-    COMMANDS.insert(2, "{0} bootstrap.py -d".format(path.join("parts", "python", "bin",
+    COMMANDS.insert(2, "{0} bootstrap.py -d -v 1.6.3".format(path.join("parts", "python", "bin",
                                                              "python{0}".format('.exe' if name == 'nt' else ''))))
     COMMANDS.insert(2, "{0} -s install isolated-python".format(BUILDOUT))
 
