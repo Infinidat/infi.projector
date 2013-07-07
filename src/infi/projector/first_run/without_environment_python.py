d52ce599fcd78eb1ebc44ff107158f2a0b497b05
Guy Rozendorn <guy@rzn.co.il>
2013-07-07 11:13:36 +0300

    HOSTDEV-1083 projector devenv build does not pass -d to distribute
    ================================================================================
    
    Host Infrastructure / HOSTDEV-1083
    new bootstrap
    
    Type:              Sub-task                          Status:        Resolved                   Assignee: Guy Rozendorn
    Priority:          Minor                             Resolution:    Fixed                      Reporter: Guy Rozendorn
    Affects Version/s:                                   Fix Version/s: 0.30
    Components: packaging                                                                          Created: 2013-07-06 20:05
    Labels:                                                                                        Updated: 2013-07-07 11:07
    
    Issue Links:
    
    Sub-Tasks:
    
    Description:
    None
    
    Comments:

--- src/infi/projector/first_run/without_environment_python.py
+++ src/infi/projector/first_run/without_environment_python.py
@@ -5,12 +5,12 @@ USE_ISOLATED_PYTHON = '--use-isolated-python' in sys.argv
 IN_VIRTUALENV = hasattr(sys, 'real_prefix')
 BUILDOUT = path.join("bin", "buildout")
 COMMANDS = [
-            "python {0} bootstrap.py -d".format('' if IN_VIRTUALENV else '-S'),
+            "python {0} bootstrap.py".format('' if IN_VIRTUALENV else '-S'),
             "{0} buildout:develop= install setup.py __version__.py".format(BUILDOUT),
             "{0} install development-scripts".format(BUILDOUT)
            ]
 if USE_ISOLATED_PYTHON:
-    COMMANDS.insert(2, "{0} bootstrap.py -d".format(path.join("parts", "python", "bin",
+    COMMANDS.insert(2, "{0} bootstrap.py".format(path.join("parts", "python", "bin",
                                                              "python{0}".format('.exe' if name == 'nt' else ''))))
     COMMANDS.insert(2, "{0} install isolated-python".format(BUILDOUT))
 
