591e2bab412d61b32b3cd3a98fc8cb167472bcb7
Guy Rozendorn <guy@rzn.co.il>
2017-02-06 09:15:00 +0200

    HOSTDEV-2618 fixing skeleton

--- src/infi/projector/plugins/builtins/repository/skeleton/__init__.py
+++ src/infi/projector/plugins/builtins/repository/skeleton/__init__.py
@@ -1,5 +1,5 @@
 
-FILENAMES = [".gitignore", "README.md", "bootstrap.py", "buildout.cfg", "setup.in"]
+FILENAMES = [".gitignore", "README.md", "get-pip.py", "buildout.cfg", "setup.in"]
 FILENAMES_TO_UPDATE = ["bootstrap.py", "buildout.cfg", "setup.in"]
 
 def get_files():
